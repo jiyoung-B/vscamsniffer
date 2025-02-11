aa64579232f96cb55b18565b3efe7835926b540b
eunbyul2 <01028700520eb@gmail.com>
2025-02-11 17:37:53 +0900

    Add attach app

--- attach/urls.py
+++ attach/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from .views import upload_audio
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Attach 메인 페이지입니다.")
+from .views import AudioFileUploadView
 
 urlpatterns = [
-    path('', index, name='attach_index'),  # 기본 경로 추가
-    path('upload/', upload_audio, name='upload_audio'),
+    path('upload/', AudioFileUploadView.as_view(), name='audio-upload'),
 ]
--- attach/views.py
+++ attach/views.py
@@ -1,21 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import AudioFileSerializer
 
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import AudioFile
-from django.core.files.storage import default_storage
-
-@csrf_exempt
-def upload_audio(request):
-    if request.method == 'POST' and request.FILES.get('file'):
-        file = request.FILES['file']
-
-        # 확장자 검증
-        if not file.name.endswith(('.mp3', '.wav', '.ogg', '.m4a')):
-            return JsonResponse({'error': '음성 파일(mp3, wav, ogg, m4a)만 업로드 가능합니다.'}, status=400)
-
-        saved_file = default_storage.save(f'uploads/{file.name}', file)
-        audio = AudioFile.objects.create(file=saved_file)
-        return JsonResponse({'message': '파일 업로드 성공!', 'file_url': audio.file.url})
-    
-    return JsonResponse({'error': '파일을 업로드하세요.'}, status=400)
+class AudioFileUploadView(APIView):
+    def post(self, request, format=None):
+        serializer = AudioFileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
